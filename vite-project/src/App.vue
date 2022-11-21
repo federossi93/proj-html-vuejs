6716b360c0b15f803c11440527728b849912e394
federossi93 <rossifederico0993@gmail.com>
2022-11-21 12:28:14 +0100

    initial commit

---
+++ vite-project/src/App.vue
@@ -0,0 +1,14 @@
+<script >
+
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="">ciao come stai</h1>
+    
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vite-project/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import '../src/assets/style.scss'
+import '../node_modules/bootstrap'
+import App from './App.vue'
+
+createApp(App).mount('#app')
